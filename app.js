479562a7da4414cb92fe6b4820c25dc0dfaf5ece
Hysum Aql <hysumaql@Hysums-MBP.fios-router.home>
2019-07-09 14:55:34 -0400

    Added places functions

--- app.js
+++ app.js
@@ -1,10 +1,89 @@
 var express = require('express')
-
 var app = express() 
+var https = require('follow-redirects').https;
+
+
+var placeDetails = function() {
+	this.places = [];
+}
+
+
+function placeSearch(latitude, longitude, radius) {
+	https.request({
+		host: 'maps.googleapis.com',
+		path: '/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=' + radius + '&type=restaurant&key=KEY',
+		method: 'GET'},
+		PlaceResponse).end();
+}
+
+
+
+function CoordinateResponse(response) {
+	var data = "";
+	var sdata = "";
+	var latitude = "";
+	var longitude = "";
+
+	response.on('data', function(chunk) {
+		data += chunk;
+	});
+	response.on('end', function() {
+		sdata = JSON.parse(data);
+		latitude = sdata.results[0].geometry.viewport.northeast.lat;
+		longitude = sdata.results[0].geometry.viewport.northeast.lng;
+		placeSearch(latitude, longitude, 50000);
+	});
+}
+
+
+
+function getCoordinates(zipcode) {
+	https.request({
+		host: 'maps.googleapis.com',
+		path: '/maps/api/geocode/json?address=' + zipcode + '&key=KEY',
+		method: 'GET'},
+		CoordinateResponse).end();
+}
+
+
+function PlaceResponse(response) {
+	var p;
+	var data = "";
+	var sdata = "";
+	var PD = new placeDetails();
+
+	response.on('data', function(chunk) {
+		data += chunk;
+	});
+	response.on('end', function() {
+		sdata = JSON.parse(data);
+		if (sdata.status === 'OK') {
+			console.log('Status: ' + sdata.status);
+			console.log('Results: ' + sdata.results.length);
+			for (p = 0; p < sdata.results.length; p++) {
+				PD.places.push(sdata.results[p]);
+			}
+			for (r = 0; r < sdata.results.length; r++) {
+				console.log('----------------------------------------------');
+				console.log(PD.places[r].name);
+				console.log('Place ID (for Place Detail search on Google):' + PD.places[r].place_id);
+				console.log('Rating: ' + PD.places[r].rating);
+				console.log('Vicinity: ' + PD.places[r].vicinity);
+			}
+		} else {
+			console.log(sdata.status);
+		}
+	});
+}
+
+
+
 
 app.get('/', function (req, res) {
   console.log('hello') 
   res.send('Hi there')
+
+  console.log(getCoordinates('37202'))
 })
 
 
